6bec1a75040cf14c6413cee5cfc30dc0618ca17e
ThangVoChien <haivo5229@gmail.com>
2024-06-10 22:30:05 +0700

    Button

--- Catour/Assets/Script/Button/TriggerTutorial.cs
+++ Catour/Assets/Script/Button/TriggerTutorial.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Button : MonoBehaviour
+public class TriggerTutorial : MonoBehaviour
 {
+    public ButtonTutorial tutorial;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,9 @@ public class Button : MonoBehaviour
     void Update()
     {
         
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        tutorial.Trigger(other.gameObject);
+        gameObject.SetActive(false);
     }
 }
---
+++ Catour/tempefkeglm.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+void swap(int *a, int *b){
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+bool snt(int n){
+	if(n<=1) return 0;
+	int i;
+	for(i=2; i<=sqrt(n); i++){
+		if(n%i==0) return 0;
+	}
+	return 1;
+}
+
+bool fibo(int a){
+	int sum=0;
+	while(a>0){
+		sum += a%10;
+		a/=10;
+	}
+	if(sum==1 || sum==2 || sum==3 || sum==5 || sum==8 || sum==13 || sum== 21) return 1;
+	else return 0;
+}
+
+int main(){
+	int a, b;
+	scanf("%d%d", &a, &b);
+	if(a>b) swap(&a, &b);
+	int i;
+	for(i=a; i<=b; i++){
+		if(snt(i) && fibo(i)) printf("%d ", &i);
+	}
+}
