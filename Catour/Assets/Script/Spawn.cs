53f537102896cc591dc38790157cdbf8816dc42a
ThangVoChien <haivo5229@gmail.com>
2024-06-14 15:33:22 +0700

    minigame

--- Catour/Assets/Script/Spawn.cs
+++ Catour/Assets/Script/Spawn.cs
@@ -8,8 +8,9 @@ using UnityEngine.UI;
 public class SpawnObstacles : MonoBehaviour
 {
     public List<GameObject> Obstacles;
+    public float spawnMin;
     public float spawnMax;
-    public bool gameOver = false;
+    private bool gameOver = false;
     public TextMeshProUGUI gameOverText;
     // Start is called before the first frame update
     void Start()
@@ -25,7 +26,7 @@ public class SpawnObstacles : MonoBehaviour
 
     IEnumerator Spawn(){
         while(!gameOver){
-            float spawnRate = Random.Range(1, spawnMax);
+            float spawnRate = Random.Range(spawnMin, spawnMax);
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, Obstacles.Count);
             Instantiate(Obstacles[index], new Vector3(14, -3.12f + (Obstacles[index].GetComponent<BoxCollider2D>().size.y)/2, 0), Obstacles[index].transform.rotation);
